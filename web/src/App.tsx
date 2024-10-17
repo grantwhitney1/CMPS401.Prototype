935ead88d62ba34a764469738ebd91c170c11057
grantwhitney1 <grant.whitney@selu.edu>
2024-10-17 16:30:31 -0500

    annotate + simplify

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+export const App = () => {
+  // TypeScript code goes here
+
+  // App JSX (basically HTML) UI is returned here
   return (
     <div>
       <h1>CMPS 401 Prototype</h1>
     </div>
   );
-}
-
-export default App;
+};
