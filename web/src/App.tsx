e9fc48d0f6e3af528150824aff9ff6ecb28866a0
grantwhitney1 <grant.whitney@selu.edu>
2024-10-17 16:42:23 -0500

    example graph

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,18 @@
+import Plot, { PlotParams } from "react-plotly.js";
+
+const EXAMPLE_PLOT_PARAMS: PlotParams = {
+  data: [
+    {
+      x: [1, 2, 3],
+      y: [1, 2, 3],
+      type: "bar",
+      mode: "lines+markers",
+      marker: { color: "red" },
+    },
+  ],
+  layout: {},
+};
+
 export const App = () => {
   // TypeScript code goes here
 
@@ -5,6 +20,7 @@ export const App = () => {
   return (
     <div>
       <h1>CMPS 401 Prototype</h1>
+      <Plot {...EXAMPLE_PLOT_PARAMS} />
     </div>
   );
 };
