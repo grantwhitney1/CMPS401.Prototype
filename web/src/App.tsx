35c8c6ee167c012b7689a9d181c2a499deaa4f61
grantwhitney1 <grant.whitney@selu.edu>
2024-10-19 20:19:48 -0500

    functions implementation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,30 +14,61 @@ import Plot, { PlotParams } from "react-plotly.js";
 const TAB_VALUES = ["f(x)", "g(x)", "j(x)", "k(x)"];
 
 /*Functions for 1/x and cubic*/
-const calculateCubicValues = (xValues: number[], xOffset: number, yOffset: number, multiplier: number): number[] => {
-  return xValues.map(x => (Math.pow(x + xOffset, 3) * multiplier) + yOffset);
+const calculateCubicValues = (
+  xValues: number[],
+  xOffset: number,
+  yOffset: number,
+  multiplier: number
+): number[] => {
+  return xValues.map((x) => Math.pow(x + xOffset, 3) * multiplier + yOffset);
 };
 
-const calculateReciprocalValues = (xValues: number[], xOffset: number, yOffset: number, multiplier: number): number[] => {
-  return xValues.map(x => multiplier*(1/(x-xOffset))+yOffset);
+const calculateReciprocalValues = (
+  xValues: number[],
+  xOffset: number,
+  yOffset: number,
+  multiplier: number
+): number[] => {
+  return xValues.map((x) => multiplier * (1 / (x - xOffset)) + yOffset);
 };
 const xValuesTest = Array.from({ length: 800 }, (_, i) => i - 400);
-/*const yValuesTestCubic = calculateCubicValues(xValuesTest,1,1,1); */
-const xOffset = 0
-const yValuesTestReciprocal = calculateReciprocalValues(xValuesTest,xOffset,0,1); 
 
-const EXAMPLE_PLOT_PARAMS: PlotParams = {
-  data: [
-    {
-      x: xValuesTest, // These are what we populate with the OUTPUT from grain .wasm functions
-      y: yValuesTestReciprocal, //
-      type: "scatter",
-      mode: "lines",
-      marker: { color: "red" },
-    },
-  ],
-  layout: { width: 1000, height: 600 },
-};
+const xOffset = 0;
+const yValuesTestReciprocal = calculateReciprocalValues(
+  xValuesTest,
+  xOffset,
+  0,
+  1
+);
+
+const yValuesTestCubic = calculateCubicValues(xValuesTest, xOffset, 0, 1);
+
+const PLOTS: PlotParams[] = [
+  {
+    data: [
+      {
+        x: xValuesTest, // These are what we populate with the OUTPUT from grain .wasm functions
+        y: yValuesTestReciprocal, //
+        type: "scatter",
+        mode: "lines",
+        marker: { color: "red" },
+      },
+    ],
+    layout: { width: 1000, height: 600 },
+  },
+  {
+    data: [
+      {
+        x: xValuesTest, // These are what we populate with the OUTPUT from grain .wasm functions
+        y: yValuesTestCubic, //
+        type: "scatter",
+        mode: "lines",
+        marker: { color: "blue" },
+      },
+    ],
+    layout: { width: 1000, height: 600 },
+  },
+];
 
 const LABEL_PROPS: InputLabelProps = {
   fw: "100",
@@ -45,7 +76,7 @@ const LABEL_PROPS: InputLabelProps = {
 
 export const App = () => {
   // TypeScript code goes here
- // Calculate y values
+  // Calculate y values
   // App JSX (basically HTML) UI is returned here
   return (
     <Tabs defaultValue={TAB_VALUES[0]}>
@@ -56,7 +87,7 @@ export const App = () => {
           </Tabs.Tab>
         ))}
       </Tabs.List>
-      {TAB_VALUES.map((item) => (
+      {TAB_VALUES.map((item, index) => (
         <Tabs.Panel value={item}>
           <Paper m="32 auto" withBorder shadow="sm" w={1400}>
             <Stack>
@@ -68,7 +99,7 @@ export const App = () => {
                   <NumberInput labelProps={LABEL_PROPS} label="Multiplier" />
                   <InputLabel />
                 </Stack>
-                <Plot {...EXAMPLE_PLOT_PARAMS} />
+                <Plot {...PLOTS[index]} />
               </Group>
             </Stack>
           </Paper>
