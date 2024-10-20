711b94eb951c6d83a7ef738960944ffd00912618
grantwhitney1 <grant.whitney@selu.edu>
2024-10-19 22:01:01 -0500

    dynamically control fn params

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,23 +8,13 @@ import {
   Tabs,
   Text,
 } from "@mantine/core";
-import { FC } from "react";
+import { FC, useMemo, useState } from "react";
 import Plot, { PlotParams } from "react-plotly.js";
 
 const TAB_VALUES = ["f(x)", "g(x)", "j(x)", "k(x)"];
 
 const xValuesTest = Array.from({ length: 800 }, (_, i) => i - 400);
 const xOffset = 0;
-
-/*Functions for 1/x and cubic*/
-const calculateCubicValues = (
-  xValues: number[],
-  xOffset: number,
-  yOffset: number,
-  multiplier: number
-): number[] => {
-  return xValues.map((x) => Math.pow(x + xOffset, 3) * multiplier + yOffset);
-};
 
 const calculateReciprocalValues = (
   xValues: number[],
@@ -42,41 +32,51 @@ const yValuesTestReciprocal = calculateReciprocalValues(
   1
 );
 
-const yValuesTestCubic = calculateCubicValues(xValuesTest, xOffset, 0, 1);
-
-const PLOTS: PlotParams[] = [
-  {
-    data: [
-      {
-        x: xValuesTest, // These are what we populate with the OUTPUT from grain .wasm functions
-        y: yValuesTestReciprocal, //
-        type: "scatter",
-        mode: "lines",
-        marker: { color: "red" },
-      },
-    ],
-    layout: { width: 1000, height: 600 },
-  },
-  {
-    data: [
-      {
-        x: xValuesTest,
-        y: yValuesTestCubic,
-        type: "scatter",
-        mode: "lines",
-        marker: { color: "blue" },
-      },
-    ],
-    layout: { width: 1000, height: 600 },
-  },
-];
-
 const LABEL_PROPS: InputLabelProps = {
   fw: "100",
 };
 
 export const App = () => {
   // TypeScript code goes here
+  const [xOffset, setXOffset] = useState<number>(0);
+  const [yOffset, setYOffset] = useState<number>(0);
+  const [multiplier, setMultiplier] = useState<number>(0);
+
+  const cubicData = useMemo(() => {
+    return xValuesTest.map(
+      (x) => Math.pow(x + xOffset, 3) * multiplier + yOffset
+    );
+  }, [multiplier, xOffset, yOffset]);
+
+  const plots: PlotParams[] = useMemo(
+    () => [
+      {
+        data: [
+          {
+            x: xValuesTest, // These are what we populate with the OUTPUT from grain .wasm functions
+            y: cubicData, //
+            type: "scatter",
+            mode: "lines",
+            marker: { color: "red" },
+          },
+        ],
+        layout: { width: 1000, height: 600 },
+      },
+      {
+        data: [
+          {
+            x: xValuesTest,
+            y: yValuesTestReciprocal,
+            type: "scatter",
+            mode: "lines",
+            marker: { color: "blue" },
+          },
+        ],
+        layout: { width: 1000, height: 600 },
+      },
+    ],
+    [cubicData]
+  );
 
   // App JSX (basically HTML) UI is returned here
   return (
@@ -95,12 +95,27 @@ export const App = () => {
               <TextThinHeader>Plot for: {item}</TextThinHeader>
               <Group w="100%" gap={0}>
                 <Stack justify="flex-start" w={350} h={500} p="xl">
-                  <NumberInput labelProps={LABEL_PROPS} label="X-Offset" />
-                  <NumberInput labelProps={LABEL_PROPS} label="Y-Offset" />
-                  <NumberInput labelProps={LABEL_PROPS} label="Multiplier" />
+                  <NumberInput
+                    labelProps={LABEL_PROPS}
+                    defaultValue={0}
+                    onChange={(value) => setXOffset(Number(value))}
+                    label="X-Offset"
+                  />
+                  <NumberInput
+                    labelProps={LABEL_PROPS}
+                    defaultValue={0}
+                    onChange={(value) => setYOffset(Number(value))}
+                    label="Y-Offset"
+                  />
+                  <NumberInput
+                    labelProps={LABEL_PROPS}
+                    defaultValue={1}
+                    onChange={(value) => setMultiplier(Number(value))}
+                    label="Multiplier"
+                  />
                   <InputLabel />
                 </Stack>
-                <Plot {...PLOTS[index]} />
+                <Plot {...plots[index]} />
               </Group>
             </Stack>
           </Paper>
